3152df49e62f2365e5bc36d03959097ede15e995
Youngmin <gosmain@naver.com>
2022-02-23 23:25:11 +0900

    demo

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home.jsx";
+import NewProduct from "./pages/newProduct/NewProduct";
+import NewUser from "./pages/newUser/NewUser";
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
+import User from "./pages/user/User";
+import UserList from "./pages/userList/UserList";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newproduct" element={<NewProduct />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
